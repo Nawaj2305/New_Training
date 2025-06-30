3f3b49ab5c935ffde81f399807db0abfe204d9d4
Nawaj.Shaikh <nawajshaikh23@gmail.com>
2025-06-30 14:02:17 +0530

    ....

---
+++ src/Exercises/Ex1_Main_Method/Task2.java
@@ -0,0 +1,15 @@
+package Exercises.Ex1_Main_Method;
+
+
+// Calculater program
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println(10+2);  //addition
+        System.out.println(10-2);  //substraction
+        System.out.println(10*3);  //multiplication
+        System.out.println(10/3);  //substraction
+    }
+
+}
